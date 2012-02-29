de7a0dbf97a9945cd8b9c0700fed9622d5e03409
Tom Benner <tombenner@gmail.com>
2012-02-29 16:08:10 -0500

    Setting up a default value for $version->default_node

--- app/views/documentation_nodes/_version_list.php
+++ app/views/documentation_nodes/_version_list.php
@@ -6,6 +6,9 @@
 			foreach ($versions as $version) {
 				$name = $default_public_version_id == $version->id ? '__default__' : $version->name;
 				if ($current_version->id != $version->id) {
+					if (empty($version->related_node)) {
+						$version->related_node = $this->model->find_one(array('conditions' => array('documentation_version_id' => $version->id, 'local_id' => 1)));
+					}
 					echo '<li>'.$this->html->object_link($version->related_node, array('text' => $version->name, 'documentation_version_name' => $name)).'</li>';
 				}
 			}
