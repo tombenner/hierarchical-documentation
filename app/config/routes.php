78b90111470235c528110a468f26b238f544fba1
Tom Benner <tbenner@castellano.dreamhost.com>
2012-02-29 12:54:32 -0800

    Adding the default routes back in

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,6 +4,9 @@ MvcRouter::public_connect('', array('controller' => 'documentation_nodes', 'acti
 MvcRouter::public_connect('documentation/{:documentation_version_name:[\d.]+}/{:local_id:[\d]+}/.*', array('controller' => 'documentation_nodes', 'action' => 'show'));
 MvcRouter::public_connect('documentation/{:local_id:[\d]+}/.*', array('controller' => 'documentation_nodes', 'action' => 'show'));
 MvcRouter::public_connect('search', array('controller' => 'documentation_nodes', 'action' => 'search'));
+MvcRouter::public_connect('{:controller}', array('action' => 'index'));
+MvcRouter::public_connect('{:controller}/{:id:[\d]+}', array('action' => 'show'));
+MvcRouter::public_connect('{:controller}/{:action}/{:id:[\d]+}');
 
 MvcRouter::admin_ajax_connect(array('controller' => 'admin_documentation_nodes', 'action' => 'update_tree'));
 MvcRouter::admin_ajax_connect(array('controller' => 'admin_documentation_nodes', 'action' => 'preview_content'));
